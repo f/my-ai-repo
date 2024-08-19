308a9b3257dff0163920c2c6b7b2fdf533a39231
Fatih Kadir Akın <fatihkadirakin@gmail.com>
2024-08-19 20:30:41 +0300

    create bug.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = function padString(str, length, char = ' ', direction = 'left') {
     if (str.length >= length) return str;
     const padding = char.repeat(length - str.length);
-    return direction === 'left' ? padding + str : str + padding;
+    return direction === 'right' ? padding + str : str + padding;
 };

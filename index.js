17818026dd346667680fac42fb2c583f32d13c54
Fatih Kadir Akın <fatihkadirakin@gmail.com>
2024-08-19 23:42:52 +0300

    Refactor: Separate strPadLeft and strPadRight functions

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-module.exports = function padString(str, length, char = ' ', direction = 'left') {
-    if (str.length >= length) return str;
-    const padding = char.repeat(length - str.length);
-    return direction === 'right' ? padding + str : str + padding;
+module.exports = {
+    strPadLeft: function (str, length, char = ' ') {
+        if (str.length >= length) return str;
+        const padding = char.repeat(length - str.length);
+        return str + padding;
+    },
+    strPadRight: function (str, length, char = ' ') {
+        if (str.length >= length) return str;
+        const padding = char.repeat(length - str.length);
+        return padding + str;
+    }
 };

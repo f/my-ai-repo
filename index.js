fc3c59f2d5bf2b79c967f171a204ce9f0867b13b
Fatih Kadir Akın <fatihkadirakin@gmail.com>
2024-08-19 19:53:53 +0300

    Add support for left and right padding

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-module.exports = function padString(str, length, char = ' ') {
+module.exports = function padString(str, length, char = ' ', direction = 'left') {
     if (str.length >= length) return str;
     const padding = char.repeat(length - str.length);
-    return padding + str;
+    return direction === 'left' ? padding + str : str + padding;
 };

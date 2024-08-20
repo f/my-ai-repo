74aa7d0ae5fda98a74bd3e1f95852eb0f350b920
Fatih Kadir Akın <fatihkadirakin@gmail.com>
2024-08-20 08:55:12 +0300

    refactor: reduce repeated code inside padding functions while keeping them separate

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-function padStr() {
-    return function (str, length, char, direction) {
-        if (str.length >= length) return str;
-        const padding = char.repeat(length - str.length);
-        return direction === 'left'? (padding + str) : (str + padding);
-    };
+function padStr(str, length, char, isRightPad) {
+    if (str.length >= length) return str;
+    const padding = char.repeat(length - str.length);
+    return isRightPad ? str + padding : padding + str;
 }
 
 module.exports = {
-    padStr: padStr()
+    strPadLeft: function(str, length, char = ' ') {
+        return padStr(str, length, char, false);
+    },
+    strPadRight: function(str, length, char = ' ') {
+        return padStr(str, length, char, true);
+    }
 };

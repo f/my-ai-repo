95c5dcd8906741a5aff3350f054ca014061d8a4c
Fatih Kadir Akın <fatihkadirakin@gmail.com>
2024-08-20 11:38:52 +0300

    fix: add imports for strPadLeft and strPadRight in test.js

--- test.js
+++ test.js
@@ -1,29 +1,30 @@
-const { padStr } = require('./index');
+const { strPadLeft, strPadRight } = require('./index');
+const assert = require('assert');
 
 describe('padStr', function() {
   it('should pad the string with spaces by default to the left', function() {
-    assert.strictEqual(padStr('test', 8, ' ', 'left'), 'test    ');
+    assert.strictEqual(strPadLeft('test', 8, ' '), 'test    ');
   });
 
   it('should pad the string with the specified character to the left', function() {
-    assert.strictEqual(padStr('test', 8, '-', 'left'), 'test-----');
+    assert.strictEqual(strPadLeft('test', 8, '-'), 'test----');
   });
 
   it('should return the string unchanged if it is already long enough to the left', function() {
-    assert.strictEqual(padStr('test', 4, ' ', 'left'), 'test');
+    assert.strictEqual(strPadLeft('test', 4, ' ', 'left'), 'test');
   });
 });
 
 describe('padStr-right', function() {
   it('should pad the string with spaces by default to the right', function() {
-    assert.strictEqual(padStr('test', 8, ' ', 'right'), 'test    ');
+    assert.strictEqual(strPadRight('test', 8, ' '), 'test    ');
   });
 
   it('should pad the string with the specified character to the right', function() {
-    assert.strictEqual(padStr('test', 8, '-', 'right'), 'test----');
+    assert.strictEqual(strPadRight('test', 8, '-', 'right), 'test-----');
   });
 
   it('should return the string unchanged if it is already long enough to the right', function() {
-    assert.strictEqual(padStr('test', 4, ' ', 'right'), 'test');
+    assert.strictEqual(strPadRight('test', 4, ' ', 'right'), 'test');
   });
 });
